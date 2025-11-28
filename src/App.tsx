6e72014f0da5dd77ad9f305788c6f69f911b90e9
njara <njara@ezway-technology.com>
2025-11-28 21:37:57 +0300

    chore: add React Router v7 future flags
    - Enable v7_startTransition and v7_relativeSplatPath to suppress warnings

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,12 @@ const App = () => (
       <Toaster />
       <Sonner />
       <AuthProvider>
-        <BrowserRouter>
+        <BrowserRouter
+          future={{
+            v7_startTransition: true,
+            v7_relativeSplatPath: true,
+          }}
+        >
           <Routes>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/admin/*" element={<AdminApp />} />
