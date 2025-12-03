28a32b16e012af89d4579f7ac4773a47473f8ea1
njara <njara@ezway-technology.com>
2025-12-03 17:16:19 +0300

    feat: Implement conditional rendering for the 'owners' resource based on super admin permissions.

--- src/admin/AdminApp.tsx
+++ src/admin/AdminApp.tsx
@@ -13,6 +13,7 @@ import {
     Psychology,
     Business
 } from '@mui/icons-material';
+import { isSuperAdmin } from '@/constants/roles';
 
 import { dataProvider as apiDataProvider } from './providers/dataProvider';
 import { authProvider } from './providers/authProvider';
@@ -60,105 +61,111 @@ export const AdminApp: React.FC = () => {
             title="Administration - Gestion du Bétail"
             {...defaultOptions}
         >
-            <Resource
-                name="cattle"
-                list={CattleList}
-                edit={CattleEdit}
-                create={CattleCreate}
-                show={CattleShow}
-                icon={Agriculture}
-                options={{ label: 'Bovins' }}
-            />
-            <Resource
-                name="users"
-                list={UserList}
-                edit={UserEdit}
-                create={UserCreate}
-                show={UserShow}
-                icon={People}
-                options={{ label: 'Utilisateurs' }}
-            />
-            <Resource
-                name="owners"
-                list={OwnerList}
-                edit={OwnerEdit}
-                create={OwnerCreate}
-                show={OwnerShow}
-                icon={Business}
-                options={{ label: 'Propriétaires' }}
-            />
-            <Resource
-                name="veterinarians"
-                list={VeterinarianList}
-                edit={VeterinarianEdit}
-                create={VeterinarianCreate}
-                show={VeterinarianShow}
-                icon={MedicalServices}
-                options={{ label: 'Intervenants' }}
-            />
-            <Resource
-                name="medicaments"
-                list={MedicamentList}
-                edit={MedicamentEdit}
-                create={MedicamentCreate}
-                show={MedicamentShow}
-                icon={Medication}
-                options={{ label: 'Médicaments' }}
-            />
-            <Resource
-                name="typeEvenements"
-                list={TypeEvenementList}
-                edit={TypeEvenementEdit}
-                create={TypeEvenementCreate}
-                show={TypeEvenementShow}
-                icon={EventNote}
-                options={{ label: 'Types d\'événements' }}
-            />
-            <Resource
-                name="events"
-                list={EvenementList}
-                edit={EvenementEdit}
-                create={EvenementCreate}
-                show={EvenementShow}
-                icon={Event}
-                options={{ label: 'Historique des événements' }}
-            />
-            <Resource
-                name="treatments"
-                list={TraitementList}
-                edit={TraitementEdit}
-                create={TraitementCreate}
-                show={TraitementShow}
-                icon={Healing}
-                options={{ label: 'Historique des traitements' }}
-            />
-            <Resource
-                name="categories"
-                list={CategoryList}
-                edit={CategoryEdit}
-                create={CategoryCreate}
-                show={CategoryShow}
-                icon={Category}
-                options={{ label: 'Catégories' }}
-            />
-            <Resource
-                name="status"
-                list={StatusList}
-                edit={StatusEdit}
-                create={StatusCreate}
-                show={StatusShow}
-                icon={Flag}
-                options={{ label: 'Statuts' }}
-            />
-            <Resource
-                name="characters"
-                list={CharactersList}
-                edit={CharactersEdit}
-                create={CharactersCreate}
-                show={CharactersShow}
-                icon={Psychology}
-                options={{ label: 'Caractères' }}
-            />
+            {(permissions) => (
+                <>
+                    <Resource
+                        name="cattle"
+                        list={CattleList}
+                        edit={CattleEdit}
+                        create={CattleCreate}
+                        show={CattleShow}
+                        icon={Agriculture}
+                        options={{ label: 'Bovins' }}
+                    />
+                    <Resource
+                        name="users"
+                        list={UserList}
+                        edit={UserEdit}
+                        create={UserCreate}
+                        show={UserShow}
+                        icon={People}
+                        options={{ label: 'Utilisateurs' }}
+                    />
+                    {isSuperAdmin(permissions) && (
+                        <Resource
+                            name="owners"
+                            list={OwnerList}
+                            edit={OwnerEdit}
+                            create={OwnerCreate}
+                            show={OwnerShow}
+                            icon={Business}
+                            options={{ label: 'Propriétaires' }}
+                        />
+                    )}
+                    <Resource
+                        name="veterinarians"
+                        list={VeterinarianList}
+                        edit={VeterinarianEdit}
+                        create={VeterinarianCreate}
+                        show={VeterinarianShow}
+                        icon={MedicalServices}
+                        options={{ label: 'Intervenants' }}
+                    />
+                    <Resource
+                        name="medicaments"
+                        list={MedicamentList}
+                        edit={MedicamentEdit}
+                        create={MedicamentCreate}
+                        show={MedicamentShow}
+                        icon={Medication}
+                        options={{ label: 'Médicaments' }}
+                    />
+                    <Resource
+                        name="typeEvenements"
+                        list={TypeEvenementList}
+                        edit={TypeEvenementEdit}
+                        create={TypeEvenementCreate}
+                        show={TypeEvenementShow}
+                        icon={EventNote}
+                        options={{ label: 'Types d\'événements' }}
+                    />
+                    <Resource
+                        name="events"
+                        list={EvenementList}
+                        edit={EvenementEdit}
+                        create={EvenementCreate}
+                        show={EvenementShow}
+                        icon={Event}
+                        options={{ label: 'Historique des événements' }}
+                    />
+                    <Resource
+                        name="treatments"
+                        list={TraitementList}
+                        edit={TraitementEdit}
+                        create={TraitementCreate}
+                        show={TraitementShow}
+                        icon={Healing}
+                        options={{ label: 'Historique des traitements' }}
+                    />
+                    <Resource
+                        name="categories"
+                        list={CategoryList}
+                        edit={CategoryEdit}
+                        create={CategoryCreate}
+                        show={CategoryShow}
+                        icon={Category}
+                        options={{ label: 'Catégories' }}
+                    />
+                    <Resource
+                        name="status"
+                        list={StatusList}
+                        edit={StatusEdit}
+                        create={StatusCreate}
+                        show={StatusShow}
+                        icon={Flag}
+                        options={{ label: 'Statuts' }}
+                    />
+                    <Resource
+                        name="characters"
+                        list={CharactersList}
+                        edit={CharactersEdit}
+                        create={CharactersCreate}
+                        show={CharactersShow}
+                        icon={Psychology}
+                        options={{ label: 'Caractères' }}
+                    />
+                </>
+            )}
         </Admin>
     );
 };
